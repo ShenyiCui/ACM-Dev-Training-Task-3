af5d154c02bf0fe6519e363a90deadfdb8993212
AudreyW05 <78786071+AudreyW05@users.noreply.github.com>
2024-02-10 22:45:10 -0800

    Add tailbone of webpage

---
+++ unit2/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+
+import BlogPage from "./components/BlogPage";
+
+function App() {
+  return (
+    <>
+      <BlogPage />
+    </>
+  );
+}
+
+export default App;
